bf763201655f72c28a4e233b133f6609677cc316
Ameya Daddikar <ameyadaddikar@gmail.com>
2018-11-04 02:48:51 +0530

    Added repo link to proj

--- app/components/footer/Footer.js
+++ app/components/footer/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends Component{
         return(
             <div className="p-2 pr-5 footer">
                     <p className="text-right text-white" style={{fontSize:'2em'}}>
-                    <a rel="nofollow" className="footer_link" target="_blank" href="https://www.vecteezy.com">Vectors by vecteezy.com</a>&nbsp;&nbsp;&nbsp;&nbsp;<a target="_blank" href="#github" className="footer_link">Github Repo</a>
+                    <a rel="nofollow" className="footer_link" target="_blank" href="https://www.vecteezy.com">Vectors by vecteezy.com</a>&nbsp;&nbsp;&nbsp;&nbsp;<a target="_blank" href="https://github.com/AmeyaDaddikar/COC_DIWALI" className="footer_link">Github Repo</a>
                     </p>
             </div>
         )
